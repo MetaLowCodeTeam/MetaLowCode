b6a6b3ac840b8072cc76aa5365f90dc69a4a7dbe
王饶冀 <2152860@qq.com>
2023-07-04 10:44:11 +0800

    修改API转发

--- vue.config.js
+++ vue.config.js
@@ -16,7 +16,7 @@ module.exports = defineConfig({
 		open: false, //运行后自动打开浏览器
 		port: process.env.VUE_APP_PORT, //挂载端口
 		proxy: {
-			'/api': {
+			'/domain': {
 				target: process.env.VUE_APP_API_BASEURL,
 				//ws: true,
 				ws: false,
