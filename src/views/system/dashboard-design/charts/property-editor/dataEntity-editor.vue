ca5f3e3dda852ffd440a62fd29ede272fca75869
王饶冀 <2152860@qq.com>
2023-10-07 18:23:36 +0800

    1. 切换实体删除维度设置

--- src/views/system/dashboard-design/charts/property-editor/dataEntity-editor.vue
+++ src/views/system/dashboard-design/charts/property-editor/dataEntity-editor.vue
@@ -1,6 +1,11 @@
 <template>
     <el-form-item label="图表数据实体">
-        <el-select v-model="optionModel.dataEntity" filterable placeholder="选择实体">
+        <el-select
+            v-model="optionModel.dataEntity"
+            filterable
+            placeholder="选择实体"
+            @change="changeEntity"
+        >
             <el-option
                 :label="op.label"
                 :value="op.entityCode"
@@ -14,14 +19,41 @@
 import useCommonStore from "@/store/modules/common";
 import { storeToRefs } from "pinia";
 const { approveDialogEntityList } = storeToRefs(useCommonStore());
+import { ref, watch, onMounted } from "vue";
 defineOptions({
     name: "dataEntity-editor",
 });
+
 const props = defineProps({
     designer: Object,
     selectedWidget: Object,
     optionModel: Object,
 });
+const emits = defineEmits(["update:optionModel"]);
+let myOption = ref({});
+watch(
+    () => props.optionModel,
+    () => {
+        initchartStyle();
+    },
+    { deep: true }
+);
+onMounted(() => {
+    initchartStyle();
+});
+
+// 初始化图表样式
+const initchartStyle = () => {
+    myOption.value = {...props.optionModel};
+};
+const changeEntity = () => {
+    myOption.value.setDimensional.dimension = [];
+    myOption.value.setDimensional.metrics=[];
+    myOption.value.setDimensional.targetValue=[];
+    myOption.value.setDimensional.showFields=[];
+    myOption.value.setDimensional.dimensionRow=[];
+    myOption.value.setDimensional.dimensionCol=[];
+};
 </script>
 <style lang="scss" scoped>
 </style>
