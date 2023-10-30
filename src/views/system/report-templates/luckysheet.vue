d0d7a06433fd30ab757b36e67b5210b95a5ac407
王饶冀 <2152860@qq.com>
2023-10-30 14:29:52 +0800

    修复报表边框报错，以及默认滚动条在中间

--- src/views/system/report-templates/luckysheet.vue
+++ src/views/system/report-templates/luckysheet.vue
@@ -20,7 +20,12 @@ import { MlReport } from "vue3-manner-report";
 import "./luckysheet.scss";
 import http from "@/utils/request";
 import { saveRecord } from "@/api/crud";
-import { onBeforeMount, ref, inject } from "vue";
+import {
+    onBeforeMount,
+    ref,
+    inject,
+    shallowRef,
+} from "vue";
 import { useRouter } from "vue-router";
 const $ElMessage = inject("$ElMessage");
 const router = useRouter();
@@ -35,8 +40,50 @@ let entityCode = ref("");
 // 菜单列表
 let menuList = ref([]);
 
+let defaultSheets = shallowRef([
+    {
+        name: "Cell", //工作表名称
+        color: "", //工作表颜色
+        index: 0, //工作表索引
+        status: 1, //激活状态
+        order: 0, //工作表的下标
+        hide: 0, //是否隐藏
+        row: 40, //行数
+        column: 20, //列数
+        defaultRowHeight: 19, //自定义行高
+        defaultColWidth: 73, //自定义列宽
+        celldata: [], //初始化使用的单元格数据
+        config: {
+            merge: {}, //合并单元格
+            rowlen: {}, //表格行高
+            columnlen: {}, //表格列宽
+            rowhidden: {}, //隐藏行
+            colhidden: {}, //隐藏列
+            borderInfo: [], //边框
+            authority: {}, //工作表保护
+        },
+        scrollLeft: 0, //左右滚动条位置
+        scrollTop: 0, //上下滚动条位置
+        luckysheet_select_save: [], //选中的区域
+        calcChain: [], //公式链
+        isPivotTable: false, //是否数据透视表
+        pivotTable: {}, //数据透视表设置
+        filter_select: {}, //筛选范围
+        filter: null, //筛选配置
+        luckysheet_alternateformat_save: [], //交替颜色
+        luckysheet_alternateformat_save_modelCustom: [], //自定义交替颜色
+        luckysheet_conditionformat_save: {}, //条件格式
+        frozen: {}, //冻结行列配置
+        chart: [], //图表配置
+        zoomRatio: 1, // 缩放比例
+        image: [], //图片
+        showGridLines: 1, //是否显示网格线
+        dataVerification: {}, //数据验证配置
+    },
+]);
+
 // 传参
-let sheets = ref(null);
+let sheets = ref();
 let reportName = ref("");
 onBeforeMount(() => {
     reportConfigId.value = router.currentRoute.value.query.reportConfigId;
@@ -77,7 +124,12 @@ function getWorkBook() {
             let reportJson = JSON.parse(resData.reportJson);
             entityCode.value = resData.entityCode;
             reportName.value = resData.reportName;
-            sheets.value = reportJson;
+            // 如果是新建
+            if (!reportJson) {
+                sheets.value = defaultSheets.value;
+            } else {
+                sheets.value = reportJson;
+            }
         }
         resolve();
     });
