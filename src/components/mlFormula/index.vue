fdcd5f3bd7f365063e9c26188ad81f56fa055c74
邪七 <2152860@qq.com>
2023-11-20 15:53:40 +0800

    计算公式修复：超长省略鼠标移至应显示完成内容

--- src/components/mlFormula/index.vue
+++ src/components/mlFormula/index.vue
@@ -69,6 +69,7 @@
                         v-for="(item,inx) of fields"
                         :key="inx"
                         @click="fieldSelect(item,true)"
+                        :title="tem.fieldLabel"
                     >
                         <span class="active-type">N</span>
                         {{ item.fieldLabel }}
@@ -101,6 +102,7 @@
                 v-for="(item,inx) of fields"
                 :key="inx"
                 @click="fieldSelect(item)"
+                :title="item.fieldLabel"
             >{{ item.fieldLabel }}</div>
         </el-popover>
     </mlDialog>
