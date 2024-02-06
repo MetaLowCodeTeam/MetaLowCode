ecf2b59f470f349c921bf968370b7c8ea180420c
邪七 <2152860@qq.com>
2024-02-06 16:40:53 +0800

    临时隐藏批量编辑设置

--- src/views/customize-menu/components/More/Index.vue
+++ src/views/customize-menu/components/More/Index.vue
@@ -128,11 +128,11 @@
                     @click="treeGroupFilterIsShow = true"
                     v-if="$TOOL.checkRole('r6008')"
                 >树状分组筛选</div>
-                <div
+                <!-- <div
                     class="pl-20 item"
                     @click="openDefaultFilterDialog"
                     v-if="$TOOL.checkRole('r6008')"
-                >批量编辑设置</div>
+                >批量编辑设置</div> -->
             </template>
         </div>
         <template #reference>
