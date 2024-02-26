ffef848225d361afb8d61070235a492b4b39bae7
邪七 <2152860@qq.com>
2024-02-26 10:32:03 +0800

    1.3.17 20240226 1 修改提交审批流程工作流条件判断

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -15,7 +15,7 @@ const useCommonStore = defineStore('commonStore', () => {
     let processEntityList = ref([]);
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.3.16 20240207"
+        webVer: "1.3.17 20240226"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
