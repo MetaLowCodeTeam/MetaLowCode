bbab766e52f82ee6d188bf47532019d1ac2726fb
邪七 <2152860@qq.com>
2024-03-20 13:46:26 +0800

    1.3.34 20240320 修改版本号

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -15,7 +15,7 @@ const useCommonStore = defineStore('commonStore', () => {
     let processEntityList = ref([]);
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.3.33 20240318"
+        webVer: "1.3.34 20240320"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
