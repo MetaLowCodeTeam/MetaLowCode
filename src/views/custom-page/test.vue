b995d13218206049405a7851c2c99078ab7bc19f
邪七 <2152860@qq.com>
2024-05-14 11:54:43 +0800

    删除调试组件

--- src/views/custom-page/test.vue
+++ src/views/custom-page/test.vue
@@ -1,13 +1,13 @@
 <template>
     <!--  -->
-    <!-- <div>自定义页面{{ query }}</div> -->
-    <MlTable />
+    <div>自定义页面{{ query }}</div>
+    <!-- <MlTable /> -->
 </template>
 
 <script setup>
 import { onMounted, ref } from "vue";
 import { useRouter } from "vue-router";
-import MlTable from "@/components/mlTable/index.vue";
+// import MlTable from "@/components/mlTable/index.vue";
 
 const Router = useRouter();
 
