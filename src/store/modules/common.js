2aff7ea2824703b87b619c88aac55728b3073612
xhliu <toliuxh@foxmail.com>
2024-05-20 14:54:34 +0800

    1.5.23 20240520 修复表单设计器无法保存事件js代码的问题。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -15,7 +15,7 @@ const useCommonStore = defineStore('commonStore', () => {
     let processEntityList = ref([]);
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.5.22 20240520"
+        webVer: "1.5.23 20240520"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
