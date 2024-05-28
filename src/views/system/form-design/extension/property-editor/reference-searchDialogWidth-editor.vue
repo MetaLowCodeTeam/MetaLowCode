ac9b1bbc273cfb7ba7edc9559d504be9e7f416ed
xhliu <toliuxh@foxmail.com>
2024-05-28 15:13:51 +0800

    1.5.43 20240528 1. 修复被计算公式引用的字段组件删除后无法编辑计算公式的问题； 2. 引用字段组件增加弹窗宽度属性设置。

---
+++ src/views/system/form-design/extension/property-editor/reference-searchDialogWidth-editor.vue
@@ -0,0 +1,25 @@
+<template>
+	<el-form-item :label="i18nt('extension.setting.searchDialogWidth')">
+		<el-input type="text" v-model="optionModel.searchDialogWidth"></el-input>
+	</el-form-item>
+</template>
+
+<script>
+import VisualDesign from '@/../lib/visual-design/designer.umd.js'
+
+const {i18n} = VisualDesign.VFormSDK
+
+export default {
+	name: "reference-searchDialogWidth-editor",
+	mixins: [i18n],
+	props: {
+		designer: Object,
+		selectedWidget: Object,
+		optionModel: Object,
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
