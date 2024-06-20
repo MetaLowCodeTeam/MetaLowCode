d55fa6bc20604e043d285cf0bc5068efb232d38a
邪七 <2152860@qq.com>
2024-06-20 15:34:53 +0800

    保存版本号

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -15,7 +15,7 @@ const useCommonStore = defineStore('commonStore', () => {
     let processEntityList = ref([]);
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.5.61 20240620"
+        webVer: "1.5.62 20240620"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
