bade1166efb2af802e2a28b627c5ebb770c8b2b1
邪七 <2152860@qq.com>
2024-07-02 14:56:05 +0800

    1.5.77 20240702 修改数据库导入参数

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -15,7 +15,7 @@ const useCommonStore = defineStore('commonStore', () => {
     let processEntityList = ref([]);
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.5.76 20240702"
+        webVer: "1.5.77 20240702"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
