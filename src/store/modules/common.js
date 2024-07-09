8595d46812d9f55873c8b1b315cf7dcaaeb4299f
邪七 <2152860@qq.com>
2024-07-09 16:13:27 +0800

    1.5.89 20240709 修复列表子表单编辑时不显示新增

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -15,7 +15,7 @@ const useCommonStore = defineStore('commonStore', () => {
     let processEntityList = ref([]);
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.5.88 20240709"
+        webVer: "1.5.89 20240709"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
