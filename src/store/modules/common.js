8181fb537668b74df2fae2cc05cccece453c5015
邪七 <2152860@qq.com>
2024-07-16 15:30:27 +0800

    1.5.108 20240716 修复表单设计列表子表单高度设置无效。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -15,7 +15,7 @@ const useCommonStore = defineStore('commonStore', () => {
     let processEntityList = ref([]);
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.5.107 20240716"
+        webVer: "1.5.108 20240716"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
