c7ca45282e24003d938233cc7583334eba793a2c
xhliu <toliuxh@foxmail.com>
2024-08-27 17:30:05 +0800

    1.6.38 20240827 可视化表单引擎：修复计算公式中使用的字段被删除后报错的问题。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -15,7 +15,7 @@ const useCommonStore = defineStore('commonStore', () => {
     let processEntityList = ref([]);
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.6.33 20240827"
+        webVer: "1.6.38 20240827"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
