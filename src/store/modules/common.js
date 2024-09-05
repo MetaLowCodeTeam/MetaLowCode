edb217c8384363b615460f0ab08d1ae337c2949e
xhliu <toliuxh@foxmail.com>
2024-09-05 16:45:51 +0800

    1.6.55 20240905 表单引擎更新：优化多行子表单组件。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.6.54 20240905"
+        webVer: "1.6.55 20240905"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
