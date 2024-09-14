2853c1d70ce68245f111ae5331975138d0c65254
xhliu <toliuxh@foxmail.com>
2024-09-14 15:12:20 +0800

    1.6.70 20240914 表单设计引擎：调整栅格容器的内边距。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.6.69 20240913"
+        webVer: "1.6.70 20240914"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
