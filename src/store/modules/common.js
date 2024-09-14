66643f875eb95e624a00a342299c17b398861195
xhliu <toliuxh@foxmail.com>
2024-09-14 15:57:10 +0800

    1.6.71 20240914 表单设计引擎：优化排版效果。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.6.70 20240914"
+        webVer: "1.6.71 20240914"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
