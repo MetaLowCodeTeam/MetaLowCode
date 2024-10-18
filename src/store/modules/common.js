347da80866691f347756b5ec6855e3dba6f284e0
邪七 <2152860@qq.com>
2024-10-18 15:41:59 +0800

    1.6.129 20241018 修复审批节点线节点报错时没有标红线节点

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.6.128 20241018"
+        webVer: "1.6.129 20241018"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
