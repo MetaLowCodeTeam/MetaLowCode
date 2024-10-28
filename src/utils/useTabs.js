2178fc1d516e0049dda9d90380f059d6d6eae93f
邪七 <2152860@qq.com>
2024-10-28 15:53:24 +0800

    1.6.146 20241028 处理关闭页签方法调用顺序问题

--- src/utils/useTabs.js
+++ src/utils/useTabs.js
@@ -6,29 +6,29 @@ import store from '@/store'
 
 import useViewTagsStore from "@/store/modules/viewTags";
 import useKeepAliveStore from "@/store/modules/keepAlive";
-import useIframeStore from "@/store/modules/iframe";
-import { storeToRefs } from "pinia";
+// import useIframeStore from "@/store/modules/iframe";
+// import { storeToRefs } from "pinia";
 const viewTagsStore = useViewTagsStore();
 const keepAliveStore = useKeepAliveStore();
-const iframeStore = useIframeStore();
+// const iframeStore = useIframeStore();
 const { removeViewTags, updateViewTagsTitle, getViewTags } = viewTagsStore;
 const { pushKeepLive,removeKeepLive,setRouteShow } = keepAliveStore;
-const { removeIframeList } = iframeStore;
+// const { removeIframeList } = iframeStore;
 
 
 export default {
 	//刷新标签
-	refresh() {
-		NProgress.start()
-		const route = router.currentRoute.value
-		removeKeepLive(route.name)
-		setRouteShow(false)
-		nextTick(() => {
-			pushKeepLive(route.name)
-			setRouteShow(true)
-			NProgress.done()
-		})
-	},
+	// refresh() {
+	// 	NProgress.start()
+	// 	const route = router.currentRoute.value
+	// 	removeKeepLive(route.name)
+	// 	setRouteShow(false)
+	// 	nextTick(() => {
+	// 		pushKeepLive(route.name)
+	// 		setRouteShow(true)
+	// 		NProgress.done()
+	// 	})
+	// },
 	//关闭标签
 	close(tag) {
 		const route = tag || router.currentRoute.value
@@ -44,30 +44,30 @@ export default {
 		}
 	},
 	//关闭标签后处理
-	closeNext(next) {
-		const route = router.currentRoute.value
-		removeViewTags(route);
-		removeIframeList(route)
-		removeKeepLive(route.name)
-		if(next){
-			const tagList = viewTags.value;
-			next(tagList)
-		}
-	},
+	// closeNext(next) {
+	// 	const route = router.currentRoute.value
+	// 	removeViewTags(route);
+	// 	removeIframeList(route)
+	// 	removeKeepLive(route.name)
+	// 	if(next){
+	// 		const tagList = viewTags.value;
+	// 		next(tagList)
+	// 	}
+	// },
 	//关闭其他
-	closeOther() {
-		const route = router.currentRoute.value
-		const tagList = [...viewTags.value]
-		tagList.forEach(tag => {
-			if(tag.meta&&tag.meta.affix || route.fullPath==tag.fullPath){
-				return true
-			}else{
-				this.close(tag)
-			}
-		})
-	},
+	// closeOther() {
+	// 	const route = router.currentRoute.value
+	// 	const tagList = [...viewTags.value]
+	// 	tagList.forEach(tag => {
+	// 		if(tag.meta&&tag.meta.affix || route.fullPath==tag.fullPath){
+	// 			return true
+	// 		}else{
+	// 			this.close(tag)
+	// 		}
+	// 	})
+	// },
 	//设置标题
-	setTitle(title){
-        updateViewTagsTitle(title)
-	}
+	// setTitle(title){
+    //     updateViewTagsTitle(title)
+	// }
 }
