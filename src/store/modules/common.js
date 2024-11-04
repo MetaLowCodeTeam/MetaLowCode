0393a6ff9b367eb7acedebc7ea39a60c7aa0c302
邪七 <2152860@qq.com>
2024-11-04 16:12:40 +0800

    1.6.161 20241104 修改高级筛选时间字段为空、不为空位置

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.6.159 20241104"
+        webVer: "1.6.161 20241104"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
