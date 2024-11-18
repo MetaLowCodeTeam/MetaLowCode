e8eeed99afb9a6c8cd88ac8e6579992b3dbe666c
邪七 <2152860@qq.com>
2024-11-18 10:57:04 +0800

    1.6.184 20241118 修改实体列表查询面板过滤逻辑

--- src/views/customize-menu/components/ListCustomizeQuery.vue
+++ src/views/customize-menu/components/ListCustomizeQuery.vue
@@ -1,8 +1,8 @@
 <style lang="scss" scoped>
 .ml-customize-query {
-    margin-bottom: 6px;
+	margin-bottom: 6px;
 	display: flex;
-    overflow: hidden;
+	overflow: hidden;
 	:deep(.is-horizontal) {
 		display: none !important;
 	}
@@ -17,20 +17,28 @@
 </style>
 <!-- 列表自定义查询 -->
 <template>
-	<div class="ml-customize-query" :style="{ 'height': customizeQueryHeight + 'px'}">
+	<div
+		class="ml-customize-query"
+		:style="{ height: customizeQueryHeight + 'px' }"
+	>
 		<div class="conditions-comp">
-            <mlSetConditions
-                v-model="compConditions"
-                :entityName="entityName"
-                displayedComp
-                :forbidUserModifyField="forbidUserModifyField"
-                :hideQueryMatchType="hideQueryMatchType"
-            />
+			<mlSetConditions
+				v-model="compConditions"
+				:entityName="entityName"
+				displayedComp
+				:forbidUserModifyField="forbidUserModifyField"
+				:hideQueryMatchType="hideQueryMatchType"
+			/>
 		</div>
 		<div class="conditions-action">
 			<el-dropdown trigger="click" @command="onSearch" class="pt-1">
-				<el-button type="primary" icon="ArrowDown" style="width: 80px;margin-right: 12px" plain>
-                    查询
+				<el-button
+					type="primary"
+					icon="ArrowDown"
+					style="width: 80px; margin-right: 12px"
+					plain
+				>
+					查询
 				</el-button>
 				<template #dropdown>
 					<el-dropdown-menu>
@@ -43,7 +51,9 @@
 					</el-dropdown-menu>
 				</template>
 			</el-dropdown>
-			<el-button @click="refreshAdvancedQuery"  style="width: 74px;">重置</el-button>
+			<el-button @click="refreshAdvancedQuery" style="width: 74px"
+				>重置</el-button
+			>
 		</div>
 	</div>
 	<!-- 设计查询面板 -->
@@ -56,15 +66,16 @@
 			/>
 		</div>
 		<template #footer>
-            <div class="fl pl-20">
-                <el-checkbox v-model="forbidUserModifyField">禁止用户修改字段</el-checkbox>
-                <el-checkbox v-model="hideQueryMatchType">隐藏查询匹配类型</el-checkbox>
-            </div>
+			<div class="fl pl-20">
+				<el-checkbox v-model="forbidUserModifyField"
+					>禁止用户修改字段</el-checkbox
+				>
+				<el-checkbox v-model="hideQueryMatchType"
+					>隐藏查询匹配类型</el-checkbox
+				>
+			</div>
 			<el-button @click="isShow = false">取消</el-button>
-			<el-button 
-                type="primary" 
-                @click="conditionsConfirm"
-            >
+			<el-button type="primary" @click="conditionsConfirm">
 				确认
 			</el-button>
 		</template>
@@ -79,12 +90,12 @@ const props = defineProps({
 	// 实体名称
 	entityName: { type: String, default: "" },
 	entityCode: { type: [Number, String], default: "" },
-    // 模块名称
+	// 模块名称
 	modelName: { type: String, default: "" },
 	// 查询条件
 	topSearchConfig: { type: Object, default: () => {} },
 });
-const emit = defineEmits(['queryNow', 'uploadItems'])
+const emit = defineEmits(["queryNow", "uploadItems"]);
 
 let isShow = ref(false);
 
@@ -98,27 +109,51 @@ let loading = ref(false);
 let customizeQueryHeight = ref(38);
 
 const changeCustomizeQueryHeight = (height) => {
-    customizeQueryHeight.value = height;
-}
+	customizeQueryHeight.value = height;
+};
+
+const notEmptyItems = [
+	"NL",
+	"NT",
+	"SFU",
+	"SFB",
+	"SFD",
+	"SFT",
+	"YTA",
+	"TDA",
+	"TTA",
+	"CUW",
+	"CUM",
+	"CUQ",
+	"CUY",
+	"REFD",
+	"REFU",
+];
 
 const onSearch = (command) => {
 	compConditions.value.equation = command;
-    let newCompConditions = {
-        equation: command,
-        items: compConditions.value.items.filter(el => el.value || el.value2)
-    };
-    emit('queryNow', newCompConditions)
+	let newCompConditions = {
+		equation: command,
+		items: [],
+	};
+	newCompConditions.items = compConditions.value.items.filter(
+		(el) =>
+			(el.value !== undefined && el.value !== null) ||
+			(el.value2 !== undefined && el.value2 !== null) ||
+			notEmptyItems.includes(el.op)
+	);
+	emit("queryNow", newCompConditions);
 };
 
 const refreshAdvancedQuery = () => {
-    let newCompConditions = JSON.parse(JSON.stringify(compConditions.value));
-    newCompConditions.items = [];
-    compConditions.value.items.forEach(el => {
-        el.value = "";
-        el.value2 = "";
-    })
-    emit('queryNow', newCompConditions)
-}
+	let newCompConditions = JSON.parse(JSON.stringify(compConditions.value));
+	newCompConditions.items = [];
+	compConditions.value.items.forEach((el) => {
+		el.value = "";
+		el.value2 = "";
+	});
+	emit("queryNow", newCompConditions);
+};
 
 /**
  * 设计查询面板
@@ -137,21 +172,21 @@ let hideQueryMatchType = ref(false);
 // 打开弹框
 const openDialog = () => {
 	isShow.value = true;
-    dialogConditions.value = JSON.parse(JSON.stringify(compConditions.value));
+	dialogConditions.value = JSON.parse(JSON.stringify(compConditions.value));
 };
 // 弹框条件确认
 const conditionsConfirm = async () => {
-	// 
-    let param = {
+	//
+	let param = {
 		config: JSON.stringify({
-            isDefaultQueryPanel: false,
-            forbidUserModifyField: forbidUserModifyField.value,
-            hideQueryMatchType: hideQueryMatchType.value,
-            filter: dialogConditions.value,
-        }),
+			isDefaultQueryPanel: false,
+			forbidUserModifyField: forbidUserModifyField.value,
+			hideQueryMatchType: hideQueryMatchType.value,
+			filter: dialogConditions.value,
+		}),
 		entityCode: props.entityCode,
 	};
-    loading.value = true;
+	loading.value = true;
 	let res = await layoutConfig.saveConfig(
 		props.topSearchConfig.layoutConfigId,
 		"TOP_SEARCH",
@@ -159,28 +194,25 @@ const conditionsConfirm = async () => {
 		props.modelName
 	);
 	if (res) {
-        emit('uploadItems', {
-            forbidUserModifyField: forbidUserModifyField.value,
-            hideQueryMatchType: hideQueryMatchType.value,
-            filter: dialogConditions.value,
-        })
-        isShow.value = false;
+		emit("uploadItems", {
+			forbidUserModifyField: forbidUserModifyField.value,
+			hideQueryMatchType: hideQueryMatchType.value,
+			filter: dialogConditions.value,
+		});
+		isShow.value = false;
 		ElMessage.success("设置成功");
 	}
 	loading.value = false;
 };
 
-
-
 watchEffect(() => {
-    compConditions.value = props.topSearchConfig.filter;
-    forbidUserModifyField.value = props.topSearchConfig.forbidUserModifyField;
-    hideQueryMatchType.value = props.topSearchConfig.hideQueryMatchType;
-})
-
+	compConditions.value = props.topSearchConfig.filter;
+	forbidUserModifyField.value = props.topSearchConfig.forbidUserModifyField;
+	hideQueryMatchType.value = props.topSearchConfig.hideQueryMatchType;
+});
 
 defineExpose({
 	openDialog,
-    changeCustomizeQueryHeight
+	changeCustomizeQueryHeight,
 });
 </script>
