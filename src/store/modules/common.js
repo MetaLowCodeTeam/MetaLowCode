bec86e6775fb16467fb7c1f0f6f19dad14c3d356
邪七 <2152860@qq.com>
2024-12-09 14:33:08 +0800

    1.6.234 20241209 报表组件更新至0.1.15

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.6.233 20241209"
+        webVer: "1.6.234 20241209"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
