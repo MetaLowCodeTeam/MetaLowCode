dad73f6cc64362b87508f98bd9bd2f57818582e7
xhliu <toliuxh@foxmail.com>
2024-12-18 16:34:20 +0800

    1.6.244 20241218 表单设计引擎更新：单行子表单、多行子表单增加前端分页设置属性，解决子表单数据行过多引起的性能问题。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.6.243 20241216"
+        webVer: "1.6.244 20241218"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
