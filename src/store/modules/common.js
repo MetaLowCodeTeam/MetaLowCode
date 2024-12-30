c39195b5d5a74d19017b62522f03d64ffef1b5eb
邪七 <2152860@qq.com>
2024-12-30 13:57:21 +0800

    1.6.259 20241230 修复左树右表页签切换报错

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.6.258 20241230" 
+        webVer: "1.6.259 20241230" 
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
