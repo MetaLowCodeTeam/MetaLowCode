85ebe752d4a30be4b52ac4db7c8836d6dace4b63
邪七 <2152860@qq.com>
2025-01-10 17:00:11 +0800

    1.7.17 20250110 字段列表隐藏允许修改设计表单、设计列表

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.16 20250110"
+        webVer: "1.7.17 20250110"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
