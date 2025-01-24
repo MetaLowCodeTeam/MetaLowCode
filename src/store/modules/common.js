ad69860b82e25ce9a43892722daa20b9f380bed0
邪七 <2152860@qq.com>
2025-01-24 11:32:32 +0800

    1.7.38 20250124 修改实体列表文件列预览弹窗

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.37 20250124"
+        webVer: "1.7.38 20250124"
     });
     const getEntityList = () => {
         return new Promise(async (resolve, reject) => {
