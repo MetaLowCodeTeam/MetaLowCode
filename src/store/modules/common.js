430bef906da65705d47797637f5f72f1c957ef13
邪七 <2152860@qq.com>
2025-02-13 14:54:21 +0800

    1.7.57 20250213 修复进入仪表盘设计页面后再进入实体表单设计页面报错

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.56 20250213"
+        webVer: "1.7.57 20250213"
     });
 
     const getEntityList = () => {
