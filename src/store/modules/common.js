ffd9e5d9f81ea6e3b71c470d3edd7a0c9fdc9c7f
xhliu <toliuxh@foxmail.com>
2025-02-17 11:54:42 +0800

    1.7.60 20250217 表单设计引擎更新：修复预览表单时切换禁用表单和只读查看模式报错的问题。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.59 20250214"
+        webVer: "1.7.60 20250217"
     });
 
     const getEntityList = () => {
