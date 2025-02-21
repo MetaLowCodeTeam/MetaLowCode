8b52875c9b1b80d02f9d3756eaca67df199a3484
邪七 <2152860@qq.com>
2025-02-21 16:38:37 +0800

    1.7.81 20250221 触发器高级过滤查询组件添加SQL查询

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.80 20250221"
+        webVer: "1.7.81 20250221"
     });
 
     const getEntityList = () => {
