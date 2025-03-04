44b2ec32833539312989a250f3960aedcb475c0a
邪七 <2152860@qq.com>
2025-03-04 16:59:53 +0800

    1.7.113 20250304 修复表格鼠标按住滑动无法被监听到。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.112 20250304"
+        webVer: "1.7.113 20250304"
     });
 
     const getEntityList = () => {
