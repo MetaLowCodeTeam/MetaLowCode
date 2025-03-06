1ccd1caac7ef1224d296bc3bda2080a659489e5b
邪七 <2152860@qq.com>
2025-03-06 09:23:37 +0800

    切换分支提交代码

--- src/api/plugins.js
+++ src/api/plugins.js
@@ -1,6 +1,12 @@
 import http from '@/utils/request'
 
 
+// 外部数据源测试链接
 export function testDbConnection(body) {
     return http.get('/plugins/metaDataWarehouse/outerData/testDbConnection', body);
+}
+
+// 查询模型字段
+export function queryModelField(body) {
+    return http.post('/plugins/metaDataWarehouse/outerData/queryModelField', body);
 }
--- src/views/system/data-base/components/OuterDataModel-edit.vue
+++ src/views/system/data-base/components/OuterDataModel-edit.vue
@@ -1,51 +1,150 @@
 <template>
-	<ml-dialog :title="title" v-model="isShow" appendToBody width="55%">
+	<ml-dialog 
+        :title="title" 
+        v-model="isShow" 
+        appendToBody 
+        width="55%"
+        :show-close="!loading"
+    >
 		<div v-loading="loading">
-			<el-form ref="formRef" :model="formData" :rules="rules">
-				<el-row :gutter="20">
-					<el-col :span="12">
-						<el-form-item label="模型名称" prop="modelName">
-							<el-input
-								v-model="formData.modelName"
-								placeholder="请输入模型名称"
-							/>
-						</el-form-item>
-					</el-col>
-					<el-col :span="12">
-						<el-form-item label="数据源" prop="dataSourceLabel">
-							<el-input
-								v-model="formData.dataSourceLabel"
-								placeholder="请选择数据源"
-							>
-								<template #append>
-									<el-button
-										type="primary"
-										@click="openDataSourceDialog"
-										icon="Search"
+			<ml-scrollbar height="500px">
+				<el-form ref="formRef" :model="formData" :rules="rules">
+					<el-row :gutter="20">
+						<el-col :span="12">
+							<el-form-item label="模型名称" prop="modelName">
+								<el-input
+									v-model="formData.modelName"
+									placeholder="请输入模型名称"
+								/>
+							</el-form-item>
+						</el-col>
+						<el-col :span="12">
+							<el-form-item label="数据源" prop="dataSourceLabel">
+								<el-input
+									v-model="formData.dataSourceLabel"
+									placeholder="请选择数据源"
+								>
+									<template #append>
+										<el-button
+											type="primary"
+											@click="openDataSourceDialog"
+											icon="Search"
+										/>
+									</template>
+								</el-input>
+							</el-form-item>
+						</el-col>
+						<el-col :span="24">
+							<el-form-item label="是否禁用" prop="isDisabled">
+								<el-radio-group v-model="formData.isDisabled">
+									<el-radio :label="true">是</el-radio>
+									<el-radio :label="false">否</el-radio>
+								</el-radio-group>
+							</el-form-item>
+						</el-col>
+						<el-col :span="24">
+							<el-form-item label="SQL文本" prop="sqlText">
+								<div class="w-100">
+									<mlCodeEditor
+										v-model="formData.sqlText"
+										mode="sql"
+										height="200"
 									/>
-								</template>
-							</el-input>
-						</el-form-item>
-					</el-col>
-					<el-col :span="24">
-						<el-form-item label="是否禁用" prop="isDisabled">
-							<el-radio-group v-model="formData.isDisabled">
-								<el-radio :label="true">是</el-radio>
-								<el-radio :label="false">否</el-radio>
-							</el-radio-group>
-						</el-form-item>
-					</el-col>
-					<el-col :span="24">
-						<el-form-item label="sql语句" prop="sqlText">
-							<el-input v-model="formData.sqlText" />
-						</el-form-item>
-					</el-col>
-				</el-row>
-			</el-form>
+								</div>
+								<div class="mt-10">
+									<el-button type="success" @click="testSql">
+										SQL解析
+									</el-button>
+								</div>
+							</el-form-item>
+						</el-col>
+						<el-col :span="24">
+							<el-form-item label="SQL参数">
+								<el-table
+									:data="formData.sqlParams"
+									:border="true"
+								>
+									<el-table-column
+										prop="name"
+									>
+                                        <template #header>
+                                            <span class="is-required">*</span>
+                                            参数名称
+                                        </template>
+                                        <template #default="scope">
+                                            <el-input v-model="scope.row.name" />
+                                        </template>
+                                    </el-table-column>
+                                    <el-table-column
+										prop="code"
+									>
+                                        <template #header>
+                                            <span class="is-required">*</span>
+                                            参数编码
+                                        </template>
+                                        <template #default="scope">
+                                            <el-input v-model="scope.row.name" />
+                                        </template>
+                                    </el-table-column>
+									<el-table-column
+										prop="type"
+									>
+                                        <template #header>
+                                            <span class="is-required">*</span>
+                                            参数类型
+                                        </template>
+                                        <template #default="scope">
+                                            <el-input v-model="scope.row.name" />
+                                        </template>
+                                    </el-table-column>
+									<el-table-column
+										prop="isRequired"
+									>
+                                        <template #header>
+                                            <span class="is-required">*</span>
+                                            必填
+                                        </template>
+                                        <template #default="scope">
+                                            <el-input v-model="scope.row.name" />
+                                        </template>
+                                    </el-table-column>
+									<el-table-column
+										prop="defaultValue"
+										label="默认值"
+									/>
+								</el-table>
+							</el-form-item>
+						</el-col>
+						<el-col :span="24">
+							<el-form-item label="查询字段">
+								<el-table
+									:data="formData.queryFields"
+									:border="true"
+								>
+									<el-table-column
+										prop="name"
+										label="字段名"
+									/>
+									<el-table-column
+										prop="label"
+										label="显示名称"
+									/>
+									<el-table-column prop="type" label="类型" />
+								</el-table>
+							</el-form-item>
+						</el-col>
+					</el-row>
+				</el-form>
+			</ml-scrollbar>
 		</div>
 		<template #footer>
-			<el-button @click="cancel">取消</el-button>
-			<el-button type="primary" @click="confirm" icon="Select">
+			<el-button @click="cancel" :loading="loading">取消</el-button>
+			<el-button
+				type="primary"
+				@click="confirm"
+				icon="Select"
+				:loading="loading"
+			>
 				保存
 			</el-button>
 		</template>
@@ -74,6 +173,15 @@
 import { ref } from "vue";
 // 引用组件
 import ReferenceSearchTable from "@/components/mlReferenceSearch/reference-search-table.vue";
+// 代码编辑器
+import mlCodeEditor from "@/components/mlCodeEditor/index.vue";
+// 滚动条
+import mlScrollbar from "@/components/mlScrollbar/index.vue";
+// 消息提示
+import { ElMessage } from "element-plus";
+// 接口
+import { queryModelField } from "@/api/plugins";
+
 
 let loading = ref(false);
 let isShow = ref(false);
@@ -82,11 +190,11 @@ let detailId = ref("");
 let entityName = ref("");
 
 let formData = ref({
-	modelName: "",
-	dataSource: "",
-	dataSourceLabel: "",
+	modelName: "1",
+	dataSource: "0000065-2f1483eaf1da41efa2224cbe5c64f7af",
+	dataSourceLabel: "本机3",
 	isDisabled: false,
-    sqlText:"",
+	sqlText: "SELECT * FROM t_user where userName like :name ",
 });
 
 let rules = ref({
@@ -94,7 +202,10 @@ let rules = ref({
 	dataSourceLabel: [
 		{ required: true, message: "请选择数据源", trigger: "blur" },
 	],
-	isDisabled: [{ required: true, message: "请选择是否禁用", trigger: "blur" }],
+	isDisabled: [
+		{ required: true, message: "请选择是否禁用", trigger: "blur" },
+	],
+	sqlText: [{ required: true, message: "请输入SQL语句", trigger: "blur" }],
 });
 
 const openDialog = (data) => {
@@ -113,7 +224,25 @@ const formRef = ref();
 const confirm = () => {
 	formRef.value.validate((valid) => {
 		if (valid) {
-			console.log(formData.value, "formData");
+			// console.log(formData.value, "formData");
+		}
+	});
+};
+
+// 测试SQL
+const testSql = () => {
+	formRef.value.validate(async (valid) => {
+		if (valid) {
+            loading.value = true;
+            let params = {
+                sqlText: formData.value.sqlText,
+                sourceId: formData.value.dataSource,
+            }
+            let res = await queryModelField(params);
+            if(res && res.code == 200) {
+                ElMessage.success("SQL解析成功");
+            } 
+            loading.value = false;
 		}
 	});
 };
@@ -124,6 +253,7 @@ const openDataSourceDialog = () => {
 };
 
 const recordSelected = (record) => {
+	console.log(record, "record");
 	formData.value.dataSource = record.id;
 	formData.value.dataSourceLabel = record.label;
 	showReferenceDialogFlag.value = false;
@@ -133,3 +263,11 @@ defineExpose({
 	openDialog,
 });
 </script>
+<style lang="scss" scoped>
+:deep(.el-form-item) {
+	align-items: initial !important;
+}
+.is-required {
+    color: #f56c6c;
+}
+</style>
