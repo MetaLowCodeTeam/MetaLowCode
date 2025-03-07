7c863b9ac46947ec1c78765fad4af1c71b0ca4a8
xhliu <toliuxh@foxmail.com>
2025-03-07 15:45:49 +0800

    1.7.121 20250307 表单设计引擎更新 1. 增加表单虚拟字段（关联展示字段）支持； 2. 新增表格子表单容器。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.120 20250306"
+        webVer: "1.7.121 20250307"
     });
 
     const getEntityList = () => {
