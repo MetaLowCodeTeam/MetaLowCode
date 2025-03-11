188a9f53f4fa7c21d96457deab8b1927dbb00650
邪七 <2152860@qq.com>
2025-03-11 16:57:27 +0800

    1.7.134 20250311 格式化表单引用组件在位搜索列表内容

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.133 20250311"
+        webVer: "1.7.134 20250311"
     });
 
     const getEntityList = () => {
