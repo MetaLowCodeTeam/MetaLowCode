4afcb2c137a1d598b71f5c1a6e21b001b34c45be
xhliu <toliuxh@foxmail.com>
2025-03-12 18:37:19 +0800

    1.7.139 20250312表单设计引擎更新 修复地区选择组件不能正常回显数据的问题。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.138 20250312"
+        webVer: "1.7.139 20250312"
     });
 
     const getEntityList = () => {
