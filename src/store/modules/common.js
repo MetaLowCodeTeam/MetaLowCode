de1155800dc5807b4061e4261fd857e191b55ec7
邪七 <2152860@qq.com>
2025-03-24 15:23:54 +0800

    1.7.169 20250324 自定义编辑弹框添加reload方法，文档同步更新

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.168 20250324"
+        webVer: "1.7.169 20250324"
     });
 
     const getEntityList = () => {
