54f2208ac6a05bf86ee953ef5aec1dc72a37308f
邪七 <2152860@qq.com>
2025-03-25 10:09:28 +0800

    切换分支提交

--- src/api/plugins.js
+++ src/api/plugins.js
@@ -1,13 +1,22 @@
 import http from "@/utils/request"
 
-
+// 保存租户
 export function saveTenantRecord(entity, recordId, formModel) {
     return http.post('/plugins/metaTenant/tenant/saveRecord', formModel, {
         params: { entity, recordId },
     })
 }
-
+    
+// 删除租户
 export function deleteTenantRecord(recordId) {
-    return http.post('/plugins/metaTenant/tenant/deleteRecord', recordId)
+    return http.post('/plugins/metaTenant/tenant/deleteRecord', null, {
+        params: { recordId }
+    })
 }
 
+// 初始化租户数据库
+export function initializationDatabase(recordId) {
+    return http.post('/plugins/metaTenant/tenant/initialization', null, {
+        params: { recordId }
+    })
+}
--- src/views/system/analysis-setting/Tenant.vue
+++ src/views/system/analysis-setting/Tenant.vue
@@ -21,13 +21,61 @@
 			</el-button>
 		</template>
 		<template #activeRow>
+			<el-table-column label="租户状态" :align="'center'" width="200">
+				<template #default="scope">
+					<span v-if="scope.row.tenantState.value == 1">
+						<el-tag type="info">
+							{{ scope.row.tenantState.label }}
+						</el-tag>
+					</span>
+					<span v-else-if="scope.row.tenantState.value == 2">
+						<el-tag type="success">
+							{{ scope.row.tenantState.label }}
+						</el-tag>
+					</span>
+					<span v-else class="ml-a-span">
+						<el-tag type="danger">
+							{{ scope.row.tenantState.label }}
+						</el-tag>
+						<el-tooltip content="点击初始化数据库" placement="top">
+							<span
+								class="ml-5 icon-top-2"
+								@click="initDatabase(scope.row)"
+							>
+								<el-icon>
+									<Coin />
+								</el-icon>
+							</span>
+						</el-tooltip>
+					</span>
+				</template>
+			</el-table-column>
 			<el-table-column
 				label="操作"
 				fixed="right"
 				:align="'center'"
-				width="140"
+				width="200"
 			>
 				<template #default="scope">
+					<!-- <el-button
+						type="primary"
+						size="small"
+						link
+                        icon="Coin"
+						v-if="scope.row.tenantState.value == 3"
+                        @click="initDatabase(scope.row)"
+					>
+						初始化数据库
+					</el-button> -->
+					<el-button
+						size="small"
+						type="primary"
+						link
+						icon="View"
+						@click="openTenantEdit(scope.row)"
+					>
+						查看
+					</el-button>
 					<el-button
 						size="small"
 						type="primary"
@@ -55,6 +103,8 @@
 <script setup>
 import { ref } from "vue";
 import TenantEdit from "./components/Tenant-Edit.vue";
+import { deleteTenantRecord, initializationDatabase } from "@/api/plugins";
+import { ElMessageBox, ElMessage } from "element-plus";
 // 默认排序
 let sortFields = ref([
 	{
@@ -69,14 +119,7 @@ let tableColumn = ref([
 		label: "租户名称",
 		align: "center",
 	},
-	{
-		prop: "tenantState",
-		label: "租户状态",
-		align: "center",
-		formatter: (row) => {
-			return row.tenantState?.label;
-		},
-	},
+
 	{
 		prop: "tenantCode",
 		label: "租户编码",
@@ -90,6 +133,14 @@ let tableColumn = ref([
 		isNegation: true,
 		width: 80,
 	},
+	// {
+	// 	prop: "tenantState",
+	// 	label: "租户状态",
+	// 	align: "center",
+	// 	formatter: (row) => {
+	// 		return row.tenantState.value == 2 ? `<span style="color: red;">{{ row.tenantState?.label }}</span>` : row.tenantState?.label;
+	// 	},
+	// },
 ]);
 
 let tenantEditRef = ref("");
@@ -112,8 +163,39 @@ const changeSwitch = async (row) => {
 };
 // 删除
 const deleteTenant = async (row) => {
-	// mlSingleListRef.value.loading = true;
-	// await tenantEditRef.value.onSave(tempForm);
-	// mlSingleListRef.value.loading = false;
+	ElMessageBox.confirm("是否确认删除?", "提示：", {
+		confirmButtonText: "确认",
+		cancelButtonText: "取消",
+		type: "warning",
+	})
+		.then(async () => {
+			mlSingleListRef.value.loading = true;
+			let res = await deleteTenantRecord(row.tenantId);
+			if (res) {
+				ElMessage.success("删除成功");
+				refresh();
+			}
+			mlSingleListRef.value.loading = false;
+		})
+		.catch(() => {});
+};
+
+// 初始化数据库
+const initDatabase = async (row) => {
+	ElMessageBox.confirm("是否确认初始化数据库?", "提示：", {
+		confirmButtonText: "确认",
+		cancelButtonText: "取消",
+		type: "warning",
+	})
+		.then(async () => {
+			mlSingleListRef.value.loading = true;
+			let res = await initializationDatabase(row.tenantId);
+			if (res) {
+				ElMessage.success("系统开始初始化数据库。。。");
+				refresh();
+			}
+			mlSingleListRef.value.loading = false;
+		})
+		.catch(() => {});
 };
 </script>
