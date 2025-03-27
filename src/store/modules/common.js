f828482088cae5fd2c55d1fab02131915822a208
邪七 <2152860@qq.com>
2025-03-27 09:58:43 +0800

    1.7.178 20250327 优化详情标签页过滤条件底纹

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.177 20250326"
+        webVer: "1.7.178 20250327"
     });
 
     const getEntityList = () => {
