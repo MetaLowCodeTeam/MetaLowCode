ee75fd667ecc4c9e736d5661ef01378a3558ed08
邪七 <2152860@qq.com>
2025-03-28 16:14:43 +0800

    1.7.182 20250328 编辑组件新增defaultFormData对象，允许对表单默认赋值

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.181 20250328"
+        webVer: "1.7.182 20250328"
     });
 
     const getEntityList = () => {
