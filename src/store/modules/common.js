3441534da7599a9dd55b233ab91d28fbd1063ef2
邪七 <2152860@qq.com>
2025-04-01 14:35:13 +0800

    1.7.184 20250401 修复实体列表自定义JS错误导致列表一直加载中，新增try catch

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.183 20250328"
+        webVer: "1.7.184 20250401"
     });
 
     const getEntityList = () => {
