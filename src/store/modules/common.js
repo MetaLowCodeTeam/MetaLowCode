42d20f4672d65a9d79b2f5d480b55aaae4995928
xhliu <toliuxh@foxmail.com>
2025-04-02 21:50:13 +0800

    1.7.188 20250402 表单引擎更新 单行子表单组件增加宽度自动铺满属性。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.187 20250402"
+        webVer: "1.7.188 20250402"
     });
 
     const getEntityList = () => {
