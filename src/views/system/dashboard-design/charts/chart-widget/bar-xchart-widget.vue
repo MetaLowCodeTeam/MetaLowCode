b2b49dcf0168eab6a180013b06f2a640c46ee1ec
邪七 <2152860@qq.com>
2025-04-10 14:39:18 +0800

    完成折线图数据对接

--- src/views/system/dashboard-design/charts/chart-widget/bar-xchart-widget.vue
+++ src/views/system/dashboard-design/charts/chart-widget/bar-xchart-widget.vue
@@ -10,8 +10,12 @@
 
 <script setup>
 import myEcharts from "@/components/scEcharts/chart-widget.vue";
-import { onMounted, reactive, ref, watch } from "vue";
+import { onMounted, reactive, ref, watch, inject } from "vue";
 import { queryChartData } from "@/api/chart";
+import useChartSourceData from "@/hooks/ChartSourceData";
+const { getDataSourceData } = useChartSourceData();
+const getFormConfig = inject('getFormConfig');
+
 const props = defineProps({
     field: Object,
     designer: Object,
@@ -60,8 +64,20 @@ let option = reactive({
     ],
 });
 
-const initOption = () => {
+const initOption = async () => {
     let { options, type } = cutField.value;
+    let { dsEnabled, dsName, dataSetName } = options;
+    if(dsEnabled && dsName) {
+        loading.value = true;
+        let res = await getDataSourceData(options, getFormConfig()); 
+        if(res) {
+            let resData = dataSetName ? res[dataSetName] : res;
+            formatData(options, resData)
+        }
+        loading.value = false;
+        option.isNoData = false;
+        return;
+    }
     if (options) {
         let { dimension, metrics } = options.setDimensional;
         if (dimension.length < 1 || metrics.length < 1) {
@@ -73,12 +89,6 @@ const initOption = () => {
             cutField.value.options.chartStyle = 1;
         }
         getChartData(options, type);
-        // if (dimension.length == 1) {
-        //     formatOption(dimension, metrics);
-        // } else {
-        //     formatOption(metrics, dimension);
-        // }
-
         option.isNoData = false;
     } else {
         option.isNoData = true;
@@ -89,50 +99,52 @@ const getChartData = async (options, type) => {
     loading.value = true;
     let res = await queryChartData(options, type);
     if (res && res.data) {
-        option.yAxis.data = [...res.data.xAxis];
-        let { chartStyle, setChartConf, axisCoordinates } =
-            cutField.value.options;
-
-        // 如果不是堆叠图，并且设置了Y轴最大值
-        if (chartStyle != 3 && axisCoordinates.max > 0) {
-            option.xAxis.min = axisCoordinates.min;
-            option.xAxis.max = axisCoordinates.max;
-        } else {
-            option.xAxis = {
-                type: "value",
-            };
-        }
-        // 图例是否显示
-        option.legend = {
-            show: setChartConf.chartShow,
-            bottom: 5,
-        };
-        option.grid.bottom = setChartConf.chartShow ? "50px" : "10px";
-        option.series = res.data.series.map((el) => {
-            el.type = "bar";
-            // 堆叠图
-            el.stack =
-                chartStyle == 1
-                    ? null
-                    : chartStyle == 2
-                    ? "普通堆叠"
-                    : "百分比堆叠";
-            // 数值是否展示
-            el.label = {
-                show: setChartConf.numShow,
-                color: "#FFF",
-                formatter: function (param) {
-                    return param.value + (chartStyle == 3 ? "%" : "");
-                },
-            };
-            return el;
-        });
+        formatData(options, res.data);
         isShowEmpty.value = false;
     } else {
         isShowEmpty.value = true;
     }
     loading.value = false;
 };
+
+const formatData = (options, data) => {
+    option.yAxis.data = [...data.xAxis];
+    let { chartStyle, setChartConf, axisCoordinates } = options;
+    // 如果不是堆叠图，并且设置了Y轴最大值
+    if (chartStyle != 3 && axisCoordinates.max > 0) {
+        option.xAxis.min = axisCoordinates.min;
+        option.xAxis.max = axisCoordinates.max;
+    } else {
+        option.xAxis = {
+            type: "value",
+        };
+    }
+    // 图例是否显示
+    option.legend = {
+        show: setChartConf.chartShow,
+        bottom: 5,
+    };
+    option.grid.bottom = setChartConf.chartShow ? "50px" : "10px";
+    option.series = data.series.map((el) => {
+        el.type = "bar";
+        // 堆叠图
+        el.stack =
+            chartStyle == 1
+                ? null
+                : chartStyle == 2
+                ? "普通堆叠"
+                : "百分比堆叠";
+        // 数值是否展示
+        el.label = {
+            show: setChartConf.numShow,
+            color: "#FFF",
+            formatter: function (param) {
+                return param.value + (chartStyle == 3 ? "%" : "");
+            },
+        };
+        return el;
+    });
+}
 
 const getData = () => {
     return option;
