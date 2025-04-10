440a72cb051e91e3e536805a07f73f8520f03ff0
xhliu <toliuxh@foxmail.com>
2025-04-10 18:18:43 +0800

    1.7.207 20250410 一对一引用、多对多引用表单组件增加onBeforeDialogOpen事件。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -18,7 +18,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.206 20250410"
+        webVer: "1.7.207 20250410"
     });
 
     const getEntityList = () => {
