4b3de61db740eedda59cbd0ac35c71c77306c9e8
邪七 <2152860@qq.com>
2025-04-21 15:36:17 +0800

    1.7.228 20250421 列显示设置自定义渲染新增参数 isMobile 用于判断是否移动端  文档同步更新

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -19,7 +19,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.7.227 20250421"
+        webVer: "1.7.228 20250421"
     });
 
     const getEntityList = () => {
