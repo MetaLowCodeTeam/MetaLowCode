0539a7ee1bee128243ca1ac8468fe3ab61f564e2
邪七 <2152860@qq.com>
2025-06-06 11:52:49 +0800

    1.8.39 20250606 引入vant 同步 外部表单组件

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/display.css'
+import 'vant/lib/index.css';
 import 'vue3-manner-report/lib/style.css';
 import scui from './ml'
 import i18n from './locales'
