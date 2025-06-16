88b1ce078f1331282d0749dd6d2c33f1b478766c
邪七 <2152860@qq.com>
2025-06-16 09:44:28 +0800

    1.8.51 20250616 合并自定义按钮

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.50 20250613"
+        webVer: "1.8.51 20250616"
     });
 
     const getEntityList = () => {
