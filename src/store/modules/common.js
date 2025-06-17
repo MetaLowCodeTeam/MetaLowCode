3feb1fd610a1a763cc9800b9a968a1d7821c11f9
邪七 <2152860@qq.com>
2025-06-17 10:11:28 +0800

    1.8.54 20250617 修改版本号

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.53 20250616"
+        webVer: "1.8.54 20250617"
     });
 
     const getEntityList = () => {
