968f014eb2f547df9130b16c540e468e65e61523
邪七 <2152860@qq.com>
2025-07-17 15:07:32 +0800

    1.8.151 20250717 修复其他实体打开详情回默认以当前url拼接modeName

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.150 20250717"
+        webVer: "1.8.151 20250717"
     });
 
     // 添加登录状态管理
