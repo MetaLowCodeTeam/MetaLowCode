00219b03e21b187adcd2abd9ad9d6c6be40c453c
邪七 <2152860@qq.com>
2025-08-15 11:07:34 +0800

    1.8.217 20250815 新建弹框 保存添加（并发互斥 + finally 复位 + 可选 blur 同步等待）

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.216 20250814"
+        webVer: "1.8.217 20250815"
     });
 
     // 添加登录状态管理
