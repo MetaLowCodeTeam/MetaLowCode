da7aad872d19700ec066f258c316f5d76d6280e7
邪七 <2152860@qq.com>
2025-08-20 13:35:50 +0800

    1.8.229 20250820 修复新用户首次登录后设置缓存信息顺序错误

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.228 20250820"
+        webVer: "1.8.229 20250820"
     });
 
     // 添加登录状态管理
