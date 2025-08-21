79bb392d62be48d7a0a0d884dfca69c1d450428f
xhliu <toliuxh@foxmail.com>
2025-08-21 15:40:17 +0800

    1.8.238 20250821 表单引擎更新 修复下拉选项组件在H5模式不能正常显示选中选项的问题。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.237 20250821"
+        webVer: "1.8.238 20250821"
     });
 
     // 添加登录状态管理
