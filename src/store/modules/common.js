ece33009405f31c5c224052a9e96816ab7b4bc25
邪七 <2152860@qq.com>
2025-08-25 11:30:36 +0800

    1.8.247 20250825 修改实体列表全局样式单元格文本颜色，添加对应描述

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.246 20250825"
+        webVer: "1.8.247 20250825"
     });
 
     // 添加登录状态管理
