d6f6f03c5d25924ce8cd3f8de7ad001843917c7c
邪七 <2152860@qq.com>
2025-10-13 13:49:17 +0800

    1.8.322 20251013 修复简约主题用户管理tree高亮时样式错误

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.321 20251013"
+        webVer: "1.8.322 20251013"
     });
 
     // 添加登录状态管理
