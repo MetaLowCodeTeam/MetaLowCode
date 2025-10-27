6c8aa61dda061fa69adcff7a4ea477c0ccc5249a
邪七 <2152860@qq.com>
2025-10-27 09:19:34 +0800

    1.8.329 20251027 更新表单组件

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.328 20251024"
+        webVer: "1.8.329 20251027"
     });
 
     // 添加登录状态管理
