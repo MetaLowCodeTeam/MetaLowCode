598f5e67df4ef274a1de65f10784daa9429076a4
xhliu <toliuxh@foxmail.com>
2025-11-10 14:49:44 +0800

    1.8.341 20251110 1. 增加@liveqing/liveplayer-v3视频播放依赖包； 2. 新增自定义页面VideoPlayControlPanel.vue。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.340 20251107"
+        webVer: "1.8.341 20251110"
     });
 
     // 添加登录状态管理
