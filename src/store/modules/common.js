f4c9705027c54031ed819a78510116aaecc2aa8e
邪七 <2152860@qq.com>
2025-12-02 10:04:30 +0800

    1.8.361 20251202 修改版本号

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.360 20251201"
+        webVer: "1.8.361 20251202"
     });
 
     // 添加登录状态管理
