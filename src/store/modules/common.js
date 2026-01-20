2ce7d6c13796083862e04f44306119d638120db2
邪七 <2152860@qq.com>
2026-01-20 13:11:35 +0800

    1.8.422 20260120 1 编辑用户时，密码改为非必填。 2 开发中心-数据导入-选择导入实体新加检测新建权限判断。 3 从列表点击数据导入默认选中该实体，且隐藏选择实体下拉选项

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.421 20260117"
+        webVer: "1.8.422 20260120"
     });
 
     // 添加登录状态管理
