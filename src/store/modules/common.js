f82cc8dccf8a0811d6629cf2bddb2ff6ad1df03f
邪七 <2152860@qq.com>
2026-01-29 14:10:16 +0800

    1.8.440 20260130 修复数据导入明细实体判断错误导致无法导入明细

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "1.8.439 20260129"
+        webVer: "1.8.440 20260130"
     });
 
     // 添加登录状态管理
