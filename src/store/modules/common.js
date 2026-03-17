a2572adb9d39b00fc3e9d179ccf66a56d761d1b1
邪七 <2152860@qq.com>
2026-03-17 15:40:39 +0800

    2.0.26 20260317 数据转化明细转明细开放主从引用字段

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "2.0.25 20260313"
+        webVer: "2.0.26 20260317"
     });
 
     // 添加登录状态管理
