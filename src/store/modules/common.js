ebda1a149335d45731b3ebb23be6e97ffefcca81
邪七 <2152860@qq.com>
2026-04-01 10:52:12 +0800

    2.0.28 20260401 更新表单组件

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "2.0.27 20260331"
+        webVer: "2.0.28 20260401"
     });
 
     // 添加登录状态管理
