117b87fad1cbe14203b940555b58842a7969c134
邪七 <2152860@qq.com>
2026-06-02 15:51:37 +0800

    2.0.49 20260602 修复快捷导航判断实体权限错误。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "2.0.48 20260601"
+        webVer: "2.0.49 20260602"
     });
 
     // 添加登录状态管理
