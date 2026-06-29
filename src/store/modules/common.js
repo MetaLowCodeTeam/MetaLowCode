372b7fa7f2444e7eb9b0fc2b145831d8928888ab
邪七 <2152860@qq.com>
2026-06-29 14:48:56 +0800

    2.0.68 20260629 修复复杂审批流程线节点过滤条件级联字段设置好后再次点击数据会丢失。

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "2.0.67 20260625"
+        webVer: "2.0.68 20260629"
     });
 
     // 添加登录状态管理
