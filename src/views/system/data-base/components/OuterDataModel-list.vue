009647657dc64d95c1c55c752508dfdadd8e9a0a
邪七 <2152860@qq.com>
2026-07-28 15:52:59 +0800

    删除测试按钮

--- src/views/system/data-base/components/OuterDataModel-list.vue
+++ src/views/system/data-base/components/OuterDataModel-list.vue
@@ -153,22 +153,6 @@
 						:prop="column.prop"
 						:label="column.label"
 					/>
-                    <el-table-column
-                        label="操作"
-                        align="center"
-                        fixed="right"
-                        width="160"
-                    >
-                        <template #default="scope">
-                            <el-button
-                                type="text"
-                                icon="Edit"
-                                @click="viewRow(scope.row)"
-                            >
-                                测试按钮
-                            </el-button>
-                        </template>
-                    </el-table-column>
 				</el-table>
 			</div>
 		</el-main>
@@ -283,25 +267,6 @@ const loadModelData = async () => {
 	}
 	loading.value = false;
 };
-
-// 查询
-const viewRow = async (row) => {
-    let dataModelReportId = "0000094-fbbcd829dbd2491aba70bfe5a703111b";
-    loading.value = true;
-    try {
-        let res = await http.post("/plugins/metaDataWarehouse/outerData/modelReport/cacheMainModelData", row);
-        if (res?.code == 200 && res.data) {
-            const appPath = import.meta.env.VITE_APP_PATH;
-            let newUrl = router.resolve(appPath + "data-model-report-view?dataModelReportId=" + dataModelReportId + "&mainModelDataId=" + res.data);
-            window.open(newUrl.href);
-        } else {
-            ElMessage.error("缓存数据失败");
-        }
-    } catch (e) {
-        ElMessage.error("请求失败");
-    }
-    loading.value = false;
-}
 
 // 查询
 let queryParamsRef = ref();
