ec1c0926655ccf312f8f2575d66269b3b5e104b2
邪七 <2152860@qq.com>
2026-08-10 13:34:01 +0800

    2.0.82 20260810 自定义页面新增定制查看数据模型页面配置，可通过ID配置到导航

---
+++ src/views/custom-page/Bespoke/viewOuterDataModel.vue
@@ -0,0 +1,13 @@
+<template>
+	<OuterDataModelList v-if="outerDataModelId" />
+	<el-empty v-else description="未获取到 outerDataModelId 参数" />
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+import { useRoute } from "vue-router";
+import OuterDataModelList from "@/views/system/data-base/components/OuterDataModel-list.vue";
+
+const route = useRoute();
+const outerDataModelId = computed(() => route.query.outerDataModelId || route.params.outerDataModelId || "");
+</script>
