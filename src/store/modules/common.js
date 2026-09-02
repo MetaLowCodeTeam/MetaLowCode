1fbacd1aac60a3835c9f3eb18bea535d34ebfc08
邪七 <2152860@qq.com>
2026-09-02 11:44:57 +0800

    2.0.86 20260902 固定透视图版本号

--- src/store/modules/common.js
+++ src/store/modules/common.js
@@ -20,7 +20,7 @@ const useCommonStore = defineStore('commonStore', () => {
 
     // 系统配置
     let publicSetting = ref({
-        webVer: "2.0.85 20260831"
+        webVer: "2.0.86 20260902"
     });
 
     // 添加登录状态管理
